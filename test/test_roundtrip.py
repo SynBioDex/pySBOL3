57b0dfee2224075f8e1a31994200abad648d03fb
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-08-25 14:21:21 -0400

    Update a test that wanted Document.find()

--- test/test_roundtrip.py
+++ test/test_roundtrip.py
@@ -14,30 +14,8 @@ class TestRoundTrip(unittest.TestCase):
                                  'BBa_F2620_PoPSReceiver.turtle.sbol')
         doc = sbol3.Document()
         doc.read(sbol_path, 'turtle')
-
-        # All of this below is because we don't yet have Document.find().
-        # When we do have Document.find() we should be able to do this:
-        #
-        # range_uri = 'https://synbiohub.org/public/igem/BBa_F2620/subcomponent_3/location_1'
-        # range1 = doc.find(range_uri)
-
-        comp_uri = 'https://synbiohub.org/public/igem/BBa_F2620'
-        comp = doc.find(comp_uri)
-        self.assertIsNotNone(comp)
-        self.assertIsInstance(comp, sbol3.Component)
-        self.assertEqual(7, len(comp.features))
-        sub3 = None
-        for feature in comp.features:
-            if feature.display_id == 'subcomponent_3':
-                sub3 = feature
-                break
-        self.assertIsNotNone(sub3)
-        self.assertIsInstance(sub3, sbol3.SubComponent)
-        range1 = None
-        for loc in sub3.locations:
-            if loc.display_id == 'location_1':
-                range1 = loc
-                break
+        range_uri = 'https://synbiohub.org/public/igem/BBa_F2620/subcomponent_3/location_1'
+        range1 = doc.find(range_uri)
         self.assertIsNotNone(range1)
         self.assertEqual(55, range1.start)
         self.assertEqual(108, range1.end)
