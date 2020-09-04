a01db5b25cc2cab5339c9a1fe889ac52d2b5ecb0
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-09-04 11:35:44 -0400

    Add unit tests for the module
    Add unit tests for completeness of the data model.

---
+++ test/test_module.py
@@ -0,0 +1,45 @@
+import unittest
+
+import sbol3
+
+
+class TestModule(unittest.TestCase):
+
+    def test_data_model(self):
+        self.assertIsInstance(sbol3.Identified, type)
+        self.assertIsInstance(sbol3.TopLevel, type)
+        self.assertIsInstance(sbol3.Sequence, type)
+        self.assertIsInstance(sbol3.Component, type)
+        self.assertIsInstance(sbol3.SubComponent, type)
+        self.assertIsInstance(sbol3.ComponentReference, type)
+        # self.assertIsInstance(sbol3.LocalSubComponent, type)
+        self.assertIsInstance(sbol3.ExternallyDefined, type)
+        # self.assertIsInstance(sbol3.SequenceFeature, type)
+        self.assertIsInstance(sbol3.Range, type)
+        self.assertIsInstance(sbol3.Cut, type)
+        self.assertIsInstance(sbol3.EntireSequence, type)
+        self.assertIsInstance(sbol3.Constraint, type)
+        self.assertIsInstance(sbol3.Interaction, type)
+        self.assertIsInstance(sbol3.Participation, type)
+        self.assertIsInstance(sbol3.Interface, type)
+        self.assertIsInstance(sbol3.CombinatorialDerivation, type)
+        self.assertIsInstance(sbol3.VariableComponent, type)
+        self.assertIsInstance(sbol3.Implementation, type)
+        # self.assertIsInstance(sbol3.ExperimentalData, type)
+        self.assertIsInstance(sbol3.Model, type)
+        self.assertIsInstance(sbol3.Collection, type)
+        self.assertIsInstance(sbol3.Namespace, type)
+        self.assertIsInstance(sbol3.Experiment, type)
+        self.assertIsInstance(sbol3.Attachment, type)
+
+    def test_not_implemented(self):
+        # These classes in the data model are not yet implemented.
+        # When they do get implemented uncomment their test above
+        # and remove their test here.
+        self.assertFalse('LocalSubComponent' in dir(sbol3))
+        self.assertFalse('SequenceFeature' in dir(sbol3))
+        self.assertFalse('ExperimentalData' in dir(sbol3))
+
+
+if __name__ == '__main__':
+    unittest.main()
