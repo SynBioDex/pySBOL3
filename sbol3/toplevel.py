c352d18033a179918db762ef7526e766d226338c
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-01-20 15:20:14 -0500

    Check identity in TopLevel constructor
    Ensure that identity has a string value in the TopLevel constructor. This
    prevents passing a None, for instance, as an identity.

--- sbol3/toplevel.py
+++ sbol3/toplevel.py
@@ -6,6 +6,11 @@ from . import *
 class TopLevel(Identified):
 
     def __init__(self, identity: str, type_uri: str) -> None:
+        # Sanity check identity, which is required for a TopLevel
+        # More checking on identity happens in Identified, but Identified
+        # does not require an identity, only TopLevel does.
+        if not identity or not isinstance(identity, str):
+            raise ValueError('Identity must be a non-empty string')
         super().__init__(identity, type_uri)
         self.attachments = ReferencedObject(self, SBOL_HAS_ATTACHMENT, 0, math.inf)
 
