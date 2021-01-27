5e8a4d371d746f4497c35a023c49eea1285dc8ea
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-01-27 12:03:47 -0500

    Add a test for issue 149
    Add a unit test related to a reported issue.

--- test/test_component.py
+++ test/test_component.py
@@ -26,6 +26,24 @@ class TestComponent(unittest.TestCase):
         self.assertEqual([sbol3.SO_PROMOTER, sbol3.SO_RBS], c.roles)
         self.assertEqual([sbol3.SO_RBS], c.roles[1:])
 
+    def test_features(self):
+        # See https://github.com/SynBioDex/pySBOL3/issues/149
+        media_template = sbol3.LocalSubComponent(identity='https://sd2e.org/media_template',
+                                                 types=[sbol3.SBO_FUNCTIONAL_ENTITY])
+        media_template.name = 'media template'
+
+        media_variable = sbol3.VariableFeature(cardinality=sbol3.SBOL_ONE)
+        media_variable.variable = media_template
+
+        all_sample_templates = [media_template]
+        sample_template = sbol3.Component(identity='https://sd2e.org/measurement_template',
+                                          component_type=sbol3.SBO_FUNCTIONAL_ENTITY)
+        sample_template.name = 'measurement template'
+        sample_template.features = all_sample_templates
+        self.assertEqual(1, len(sample_template.features))
+        self.assertEqual(media_template.identity,
+                         sample_template.features[0].identity)
+
 
 if __name__ == '__main__':
     unittest.main()
