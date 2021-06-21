7f72722ecdac6f5b3c252c636dfdeafed5f693ec
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-06-21 16:34:38 -0400

    Set namespace when default is used
    Fix a bug that left namespace None when the default namespace was used
    to form the identity.

---
+++ test/test_toplevel.py
@@ -0,0 +1,24 @@
+import posixpath
+import unittest
+
+import sbol3
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        sbol3.set_defaults()
+
+    def tearDown(self) -> None:
+        sbol3.set_defaults()
+
+    def test_default_namespace(self):
+        # See https://github.com/SynBioDex/pySBOL3/issues/263
+        display_id = 'foo'
+        c = sbol3.Component(display_id, sbol3.SBO_DNA)
+        self.assertIsNotNone(c.namespace)
+        self.assertTrue(c.identity.endswith(display_id))
+        self.assertEqual(c.identity, posixpath.join(c.namespace, display_id))
+
+
+if __name__ == '__main__':
+    unittest.main()
