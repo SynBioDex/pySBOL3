7c2f03b7dc65d86dd52d6bcb429a5bbecb3f3044
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-08-04 14:37:36 -0400

    Expand string_to_display_id test

--- test/test_util.py
+++ test/test_util.py
@@ -12,9 +12,18 @@ class TestUtil(unittest.TestCase):
             ('世界', '世界'),  # Is this ok?
             ('Épée', 'Épée'),  # Is this ok?
             ('a:b', 'a_COLONb'),
+            # convert special characters
+            ('a b', 'a_b'),
+            ('a-b', 'a_b'),
+            ('a.b', 'a_b'),
+            # convert multiple special characters
+            ('this is-long.', 'this_is_long_'),
+            ('a/b', 'a_SOLIDUSb'),
+            ('a\\b', 'a_REVERSE_SOLIDUSb'),
         )
-        for input_str, actual in data:
-            self.assertEqual(actual, sbol3.string_to_display_id(input_str))
+        for input_str, expected in data:
+            actual = sbol3.string_to_display_id(input_str)
+            self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
