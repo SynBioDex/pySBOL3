462e2327189afdf3456c6843f742c800022437c6
Tom Mitchell <tcmitchell@users.noreply.github.com>
2023-03-08 09:40:30 -0500

    Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='sbol3',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
-
+            'Programming Language :: Python :: 3.11',
       ],
       # What does your project relate to?
       keywords='synthetic biology',
