9c419c89e83ea59466ddb69fa3ecdeb91bd9148b
Omar Elsherif <omarelsherif010@gmail.com>
2023-03-28 11:19:09 +0200

    Add valid and invalid functions for feature class
    This valid and invalid functions ensure that the feature(subcomponent) have the correct orientation if the user entered any

--- test/test_feature.py
+++ test/test_feature.py
@@ -10,34 +10,35 @@ class TestFeature(unittest.TestCase):
     def tearDown(self) -> None:
         sbol3.set_defaults()
 
-    def test_create(self):
-        # should I create component first and add subcomponent into it???
+    def test_valid(self):
+        # should I create component first and add subcomponent into it??? 
         # Feature is abstract base class and hence it shouldn't be called directly, only their childs could?
-
-        # >>> ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
-        # >>> circuit = sbol3.Component('circuit', sbol3.SBO_DNA, roles=[sbol3.SO_ENGINEERED_REGION])
-        # >>> ptet_sc = sbol3.SubComponent(ptet)
-        # >>> circuit.features += [ptet_sc]
-
+        ## Answer -> yes (for both questions)
         # 'this only works if we have subcomponent as a feature, so we need to generalize to all feature types'
-        ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
 
+        sbol3.set_namespace('https://github.com/synbiodex/pysbol3')
+        ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
         circuit = sbol3.Component('circuit', sbol3.SBO_DNA, roles=[sbol3.SO_ENGINEERED_REGION])
 
-        ptet_sc = sbol3.SubComponent(ptet)
+        ptet_sc = sbol3.SubComponent(ptet, orientation='https://identifiers.org/SO:0001030')  ## SO_FORWARD = 'https://identifiers.org/SO:0001030'
         circuit.features += [ptet_sc]
-       
-        # feature = sbol3.feature('feature1')
-        self.assertIsNotNone(circuit.features)
-        self.assertEqual(sbol3.SBOL_SUBCOMPONENT, ptet_sc.type_uri)
 
-    def test_orientations(self):
-        pass
+        report = ptet_sc.validate()
 
-    def test_roles(self):
-        pass
+        self.assertEqual(0, len(report))
 
-    # def test_invalid(self):
+    def test_invalid(self):
+        sbol3.set_namespace('https://github.com/synbiodex/pysbol3')
+        ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
+        circuit = sbol3.Component('circuit', sbol3.SBO_DNA, roles=[sbol3.SO_ENGINEERED_REGION])
+        
+        ptet_sc = sbol3.SubComponent(ptet, orientation='Wrong Orientation')  ## not in the valid orientation list
+        circuit.features += [ptet_sc]
+
+        report = ptet_sc.validate()
+
+        self.assertEqual(1, len(report))
+
 
     if __name__ == '__main__':
         unittest.main()
