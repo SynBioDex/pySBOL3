825f7fb5903a6506e88ef8ee78c1f404bf1930a9
Omar Elsherif <omarelsherif010@gmail.com>
2023-04-02 16:28:40 +0200

    Edit orientation parameter and comments

--- test/test_feature.py
+++ test/test_feature.py
@@ -12,15 +12,10 @@ class TestFeature(unittest.TestCase):
         sbol3.set_defaults()
 
     def test_valid(self):
-        # should I create component first and add subcomponent into it???
-        # Feature is abstract base class and hence it shouldn't be called directly, only their childs could?
-        # Answer -> yes (for both questions)
-        # 'this only works if we have subcomponent as a feature, so we need to generalize to all feature types'
         sbol3.set_namespace('https://github.com/synbiodex/pysbol3')
         ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
         circuit = sbol3.Component('circuit', sbol3.SBO_DNA, roles=[sbol3.SO_ENGINEERED_REGION])
-        ptet_sc = sbol3.SubComponent(ptet, orientation='https://identifiers.org/SO:0001030')
-        # SO_FORWARD = 'https://identifiers.org/SO:0001030'
+        ptet_sc = sbol3.SubComponent(ptet, orientation=sbol3.SO_FORWARD)
         circuit.features += [ptet_sc]
         report = ptet_sc.validate()
         self.assertEqual(0, len(report))
@@ -29,7 +24,8 @@ class TestFeature(unittest.TestCase):
         sbol3.set_namespace('https://github.com/synbiodex/pysbol3')
         ptet = sbol3.Component('pTetR', sbol3.SBO_DNA, roles=[sbol3.SO_PROMOTER])
         circuit = sbol3.Component('circuit', sbol3.SBO_DNA, roles=[sbol3.SO_ENGINEERED_REGION])
-        ptet_sc = sbol3.SubComponent(ptet, orientation='Wrong Orientation')  # not in the valid orientation list
+        # orientation should be an item from this list [SO_FORWARD, SO_REVERSE, SBOL_INLINE, SBOL_REVERSE_COMPLEMENT]
+        ptet_sc = sbol3.SubComponent(ptet, orientation='Wrong Orientation')
         circuit.features += [ptet_sc]
         report = ptet_sc.validate()
         self.assertEqual(1, len(report))
