e1daf84fe6cf8228bb370faf3c79e4cb70dc6182
Bryan Bartley <bryan.a.bartley@raytheon.com>
2025-02-21 12:21:50 -0800

    Bump version

--- sbol3/__init__.py
+++ sbol3/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2'
+__version__ = '1.3'
 
 from .constants import *
 from .config import set_defaults, get_namespace, set_namespace
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sbol3',
-      version='1.2',
+      version='1.3',
       description='Python implementation of SBOL 3 standard',
       python_requires='>=3.8',
       url='https://github.com/SynBioDex/pySBOL3',
